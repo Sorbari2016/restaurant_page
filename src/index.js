024988ebce6488d819676ca341a738e51acaa368
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2025-06-03 07:16:42 +0100

    Set  HTML template, plugins and loaders

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 console.log("Hellow World"); 
+
+import './styles.css';
+
+console.log("Webpack with CSS and HTML loaders!");
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,33 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js', 
+  entry: './src/index.js',
+
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'), // output folder
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // cleans the dist folder before each build (optional but useful)
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,      
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/i,     
+        loader: 'html-loader',
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+
   mode: 'development',
 };
